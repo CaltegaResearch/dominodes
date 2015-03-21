73df7a96c4d73291a21620bd2ff7349f3fa9368c
hDeraj <jharkins67@gmail.com>
2015-03-21 14:17:47 -0400

    editing cells and initializing

--- js/main.js
+++ js/main.js
@@ -15,26 +15,61 @@ function addCell(x,y,label,value,color){
 	cell.css("top", y);
 	cell.css("left", x);
 	cell.click(onCellClick);
+	cell.dblclick(onCellDblClick);
+	cell.on('keydown',onCellKeyDown);
 	$(".wrapper").append(cell);
+	toggleSelected(cell.get(0));
+	cell.get(0).children[0].children[0].focus();
 }
 
 function onCellClick(e){
 	e.stopPropagation();
 	toggleSelected(e.currentTarget);
 }
-
-function toggleSelected(cell){
-	if(selectedCell){
-		selectedCell.classList.remove("selected");
-	}
-	if(cell != selectedCell){
-		cell.classList.add("selected");
-		selectedCell = cell;
+function onCellDblClick(e){
+	e.stopPropagation();
+	var pointX = e.pageX - e.currentTarget.style.left.split("px")[0];
+	var pointY = e.pageY - e.currentTarget.style.top.split("px")[0];
+	if(pointX < 150){
+		e.currentTarget.children[0].children[0].focus();
 	}else{
-		selectedCell = null;
+		e.currentTarget.children[1].children[0].focus();
+	}
+}
+function onCellKeyDown(e){
+	if (e.which == 13) {
+	  	e.stopPropagation();
+	    if ("activeElement" in document){
+    		document.activeElement.blur();
+	    }
+		clearSelected();
+		return false;
 	}
 }
 
-$(".wrapper").click(function(e){
+function toggleSelected(cell){
+	var tmp = selectedCell;
+	clearSelected();
+	cell.classList.add("selected");
+	selectedCell = cell;
+}
+function clearSelected(){
+	if(selectedCell){
+		selectedCell.classList.remove("selected");
+	}
+	selectedCell = null;
+}
+
+$(".wrapper").dblclick(function(e){
 	addCell(e.pageX - 150,e.pageY - 30);
+});
+$(".wrapper").click(function(e){
+	clearSelected();
+})
+$(document).keypress(function(e) {
+  if(e.which == 13) {
+  	e.stopPropagation();
+    window.blur();
+    clearSelected();
+  }
 });
