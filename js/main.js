db4408d1ce305db4bd1891db616b5f7aef71bbce
hDeraj <jharkins67@gmail.com>
2015-03-21 13:21:55 -0400

    adding cells and selecting cells

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
+var selectedCell = null;
+
 var cellTemplate = $('#cell-template').html();
 Mustache.parse(cellTemplate);
 
-function addCell(label,value,color){
+function addCell(x,y,label,value,color){
 	var data = {
 		"label" : label,
 		"value" : value,
@@ -10,9 +12,29 @@ function addCell(label,value,color){
 
 	var rendered = Mustache.render(cellTemplate, data);
 	var cell = $(rendered).draggable();
+	cell.css("top", y);
+	cell.css("left", x);
+	cell.click(onCellClick);
 	$(".wrapper").append(cell);
 }
 
-addCell('CPC',1.35);
-addCell('sales', 'yes');
-addCell('bruh','swag');
+function onCellClick(e){
+	e.stopPropagation();
+	toggleSelected(e.currentTarget);
+}
+
+function toggleSelected(cell){
+	if(selectedCell){
+		selectedCell.classList.remove("selected");
+	}
+	if(cell != selectedCell){
+		cell.classList.add("selected");
+		selectedCell = cell;
+	}else{
+		selectedCell = null;
+	}
+}
+
+$(".wrapper").click(function(e){
+	addCell(e.pageX - 150,e.pageY - 30);
+});
