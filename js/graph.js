28306dad01423fe71d636d678bb25c603136a940
NickErrant <nmun2615@gmail.com>
2015-03-21 23:22:01 -0400

    graph merge

--- js/graph.js
+++ js/graph.js
@@ -1,3 +1,5 @@
+var math = require('mathjs');
+
 var uniqueNum = 0;
 
 var nodes = {};
@@ -45,4 +47,41 @@ function setColor(id, color){
 }
 function setComment(id, comment){
 	nodes[id].comment = comment;
+}
+
+function evalFormula(id){
+	var replace = nodes[id].formula;
+
+	nodes[id].inputs.forEach(function(cell){
+		if(cell.value === '-') return '-';
+		var re = new RegExp(' '+cell.value+' ', "gi");
+		replaced = replaced.replace(re, cell.value);
+	});
+
+	try{
+		return math.eval(replaced);
+	}
+	catch(err){
+		return '-';
+	}
+}
+
+function evalGraph(){
+	visited = [];
+	notYet = Object.keys(nodes);
+	function traverse(cell){
+
+		nodes[cell].inputs.forEach(function(next){
+			if (visited.indexOf(next) !== -1){
+				return traverse(next);
+			}
+		});
+
+		setValue(cell, evalFormula(cell));
+		visited.push(notYet.splice(notYet.indexOf(cell), 1));
+	}
+
+	while(notYet.length > 0){
+		traverse(notYet[0]);
+	}
 }
