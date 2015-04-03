c2f09c29351258e15727d26086e0389ff460f61c
hDeraj <jharkins67@gmail.com>
2015-04-03 18:37:37 -0400

    "disabling" nodes you can't add an edge to

--- js/main.js
+++ js/main.js
@@ -84,6 +84,9 @@ function clearSelectedColor(){
 }
 
 function onInputClicked(element){
+	if(selectedOutput == null){
+		return;
+	}
 	selectedInput = element.parentNode.id;
 	if(nodes[selectedOutput].outputs.indexOf(selectedInput) !== -1){
 		nodes[selectedInput].inputs.splice(nodes[selectedInput].inputs.indexOf(selectedOutput),1);
@@ -117,16 +120,30 @@ function onInputClicked(element){
 function onOutputClicked(element){
 	selectedInput = null;
 	selectedOutput = element.parentNode.id;
-	for(var node of willFormCycle(selectedOutput)){
-		// $('#'+node)
-		// jQuery/CSS stuff goes here
+	var cycleNodes = willFormCycle(selectedOutput);
+	for(var node of cycleNodes){
+		$('#'+node).addClass("disabled");
+	}
+	$(".wrapper").on("click",function(e){
+		if(e.originalEvent.originalTarget.className != "port port-right"){
+			selectedOutput = null;
+			resetDisabled();
+			$(this).off(e);
+		}
+	});
+}
+
+function resetDisabled(){
+	var keys = Object.keys(nodes);
+	for(var i=0; i<keys.length;i++){
+		$("#"+keys[i]).removeClass("disabled");
 	}
 }
 
 function onCellClick(e){
 	selectCell(e.currentTarget);
 	$("#formulaInput").focus();
-	e.stopPropagation();
+	//e.stopPropagation();
 }
 function onCellDblClick(e){
 	e.stopPropagation();
@@ -202,7 +219,10 @@ $(".wrapper").dblclick(function(e){
 	createCell(e.pageX - 150,e.pageY - 30);
 });
 $(".wrapper").click(function(e){
-	unselectCell();
+	console.log(e);
+	if(e.originalEvent.originalTarget.className == "wrapper"){
+		unselectCell();
+	}
 })
 $(document).keypress(function(e) {
 	if(e.which == 13) {
