5ea6c0df7c219dab4e037a98b49f613ddda2e51e
hDeraj <jharkins67@gmail.com>
2015-04-05 12:10:40 -0400

    single quotes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 //globals for jquery-ui
 global.document = window.document;
 global.navigator = window.navigator;
@@ -23,53 +23,53 @@ Mustache.parse(cellTemplate);
 function createCell(x,y,color,id,value){
 	//default options
 	var ID = id || addNode();
-	color = color || "grey";
+	color = color || 'grey';
 	value = value || '-';
 
 	var data = {
-		"label" : nodes[ID].label,
-		"value" : value,
-		"color" : color,
-		"top" : y + "px",
-		"left" : x + "px",
-		"id" : ID
+		'label' : nodes[ID].label,
+		'value' : value,
+		'color' : color,
+		'top' : y + 'px',
+		'left' : x + 'px',
+		'id' : ID
 	};
 
 	var rendered = Mustache.render(cellTemplate, data);
 	var cell = $(rendered)
-		.draggable({snap: true, containment: "parent"})
+		.draggable({snap: true, containment: 'parent'})
 		.bind('drag', onCellDragged)
 		.dblclick(onCellDblClick)
 		.click(onCellClick);
-	$(".wrapper").append(cell);
+	$('.wrapper').append(cell);
 
 	saveCellPos(ID);
 	selectCell(ID);
-	$("#label").focus();
-	$("#label").select();
+	$('#label').focus();
+	$('#label').select();
 }
 
 function createEdge(from,to){
-	var cellHeight = parseInt($("#"+to).css('height').split(".")[0]);
-	var cellWidth = parseInt($("#"+to).css('width').split(".")[0]);
-	var inOff = $("#"+to).offset();
-	var outOff = $("#"+from).offset();
+	var cellHeight = parseInt($('#'+to).css('height').split('.')[0]);
+	var cellWidth = parseInt($('#'+to).css('width').split('.')[0]);
+	var inOff = $('#'+to).offset();
+	var outOff = $('#'+from).offset();
 	var outX = outOff.left+cellWidth;
 	var outY = outOff.top+cellHeight/2;
 	var inX = inOff.left;
 	var inY = inOff.top+cellHeight/2;
 	var diff= (inX-outX)/2;
-	var dstr = "M"+outX+","+outY+" C"+(outX+diff)+","+outY+" "+(inX-diff)+","+inY+" "+inX+","+inY;
-	var path = "<path id=\""+from+to+"\" d=\""+dstr+"\" />";
-	var edge = document.createElementNS("http://www.w3.org/2000/svg", "path");
+	var dstr = 'M'+outX+','+outY+' C'+(outX+diff)+','+outY+' '+(inX-diff)+','+inY+" "+inX+","+inY;
+	var path = '<path id="'+from+to+'" d="'+dstr+'" />';
+	var edge = document.createElementNS('http://www.w3.org/2000/svg', 'path');
 	edge.setAttribute('d',dstr);
 	edge.setAttribute('id',from+to);
-	$("#edgesSvg").append(edge);
+	$('#edgesSvg').append(edge);
 }
 
 function saveCellPos(id){
-	nodes[id].left = (parseInt($("#"+id).css("left").split("px")[0])/VW) + "vw";
-	nodes[id].top = (parseInt($("#"+id).css("top").split("px")[0])/VW) + "vw";
+	nodes[id].left = (parseInt($('#'+id).css('left').split('px')[0])/VW) + 'vw';
+	nodes[id].top = (parseInt($('#'+id).css('top').split('px')[0])/VW) + 'vw';
 }
 
 function destroyCell(id){
@@ -80,33 +80,33 @@ function destroyCell(id){
 function onCellDragged(event, ui){
 	var id = event.currentTarget.id;
 	selectCell(id);
-	$("#formulaInput").focus();
+	$('#formulaInput').focus();
 	saveCellPos(id);
 	updateEdges(id);
 }
 
 function updateEdges(id){
-	var offset = $("#"+id).offset();
-	var cellWidth = parseInt($("#"+id).css("width").split("px")[0]);
-	var cellHeight = parseInt($("#"+id).css("height").split("px")[0]);
+	var offset = $('#'+id).offset();
+	var cellWidth = parseInt($('#'+id).css('width').split('px')[0]);
+	var cellHeight = parseInt($('#'+id).css('height').split('px')[0]);
 
 	for(let inputId of nodes[id].inputs){
 		let pathid = inputId+id;
 		let path = document.getElementById(pathid);
-		let ds = path.getAttribute("d").split(" ");
-		ds[1] = "C"+((offset.left+parseInt(ds[0].split("M")[1].split(",")[0]))/2)+","+ds[1].split(",")[1];
-		ds[2] = ((offset.left+parseInt(ds[0].split("M")[1].split(",")[0]))/2)+","+(offset.top+cellHeight/2);
-		ds[3] = offset.left+","+(offset.top+cellHeight/2);
-		path.setAttribute('d',ds.join(" "));
+		let ds = path.getAttribute('d').split(' ');
+		ds[1] = 'C'+((offset.left+parseInt(ds[0].split('M')[1].split(',')[0]))/2)+','+ds[1].split(',')[1];
+		ds[2] = ((offset.left+parseInt(ds[0].split('M')[1].split(',')[0]))/2)+','+(offset.top+cellHeight/2);
+		ds[3] = offset.left+','+(offset.top+cellHeight/2);
+		path.setAttribute('d',ds.join(' '));
 	}
 	for(let outputId of nodes[id].outputs){
 		let pathid = id+outputId;
 		let path = document.getElementById(pathid);
-		let ds = path.getAttribute("d").split(" ");
-		ds[0] = "M"+(offset.left+cellWidth)+","+(offset.top+cellHeight/2);
-		ds[1] = "C"+((offset.left+cellWidth+parseInt(ds[3].split(",")[0]))/2)+","+(offset.top+cellHeight/2);
-		ds[2] = ((offset.left+cellWidth+parseInt(ds[3].split(",")[0]))/2)+","+ds[2].split(",")[1];
-		path.setAttribute('d',ds.join(" "));
+		let ds = path.getAttribute('d').split(' ');
+		ds[0] = 'M'+(offset.left+cellWidth)+','+(offset.top+cellHeight/2);
+		ds[1] = 'C'+((offset.left+cellWidth+parseInt(ds[3].split(',')[0]))/2)+','+(offset.top+cellHeight/2);
+		ds[2] = ((offset.left+cellWidth+parseInt(ds[3].split(',')[0]))/2)+','+ds[2].split(',')[1];
+		path.setAttribute('d',ds.join(' '));
 	}
 }
 
@@ -114,9 +114,9 @@ function setSelectedColor(color){
 	if(selectedCell){
 		//clear any color class it already has
 		for(let color of COLORS){
-			$("#"+selectedCell).removeClass(color);
+			$('#'+selectedCell).removeClass(color);
 		}
-		$("#"+selectedCell).addClass(color);
+		$('#'+selectedCell).addClass(color);
 		setColor(selectedCell,color);
 	}
 }	
@@ -131,7 +131,7 @@ function onInputClicked(id){
 		//edge exists: need to remove
 		nodes[selectedInput].inputs.splice(nodes[selectedInput].inputs.indexOf(selectedOutput),1);
 		nodes[selectedOutput].outputs.splice(nodes[selectedOutput].outputs.indexOf(selectedInput),1);
-		$("#"+selectedOutput+selectedInput).remove();
+		$('#'+selectedOutput+selectedInput).remove();
 	}
 	else if(!willFormCycle(selectedOutput).has(selectedInput)){
 		//edge doesn't exist and doesn't form cycle: need to add
@@ -151,14 +151,15 @@ function onOutputClicked(id){
 	var cycleNodes = willFormCycle(selectedOutput);
 	//add disabled classes to nodes that will form a cycle
 	for(let node of cycleNodes){
-		$('#'+node).addClass("disabled");
+		$('#'+node).addClass('disabled');
 	}
 
 	//temp function that removes disabled classes for cycle detection
-	$(".wrapper").on("click",function(e){
-		if(e.originalEvent.target.className !== "port port-right"){
+	$('.wrapper').on('click',function(e){
+		if(e.originalEvent.target.className.indexOf('port-right') > -1){
 			selectedOutput = null;
 			resetDisabled();
+			//remove this click event
 			$(this).off(e);
 		}
 	});
@@ -166,13 +167,13 @@ function onOutputClicked(id){
 
 function resetDisabled(){
 	for(let id of Object.keys(nodes)){
-		$("#"+id).removeClass("disabled");
+		$('#'+id).removeClass('disabled');
 	}
 }
 
 function onCellClick(e){
 	selectCell(e.currentTarget.id);
-	$("#formulaInput").focus();
+	$('#formulaInput').focus();
 }
 function onCellDblClick(e){
 	e.stopPropagation();
@@ -180,93 +181,94 @@ function onCellDblClick(e){
 
 function selectCell(id){
 	if(selectedCell){
-		$("#"+selectedCell).removeClass("selected");
+		$('#'+selectedCell).removeClass('selected');
 	}
-	$("#"+id).addClass("selected");
+	$('#'+id).addClass('selected');
 	selectedCell = id;
 	loadSideBar(selectedCell);
 }
 
 function unselectCell(){
 	if(selectedCell){
-		$("#"+selectedCell).removeClass("selected");
+		$('#'+selectedCell).removeClass('selected');
 	}
 }
 
 function addToFormula(text){
-	var cursorPos = document.getElementById("formulaInput").selectionStart;
+	var cursorPos = document.getElementById('formulaInput').selectionStart;
 	var id = selectedCell;
 	var oldFormula = nodes[id].formula;
+	//insert text at cursor
 	var newFormula = oldFormula.substring(0,cursorPos) +
 					 text +
 					 oldFormula.substring(cursorPos);
 
 	setFormula(id,newFormula);
-	$("#formulaInput").val(newFormula);
-	$("#formulaInput").focus();
+	$('#formulaInput').val(newFormula);
+	$('#formulaInput').focus();
 }
 
 function clearSideBar(){
-	$("#formulaInput").val("");
-	$("#label").val("");
-	$("#inputsList").html("");
-	$("#formulaInput").blur();
+	$('#formulaInput').val('');
+	$('#label').val('');
+	$('#inputsList').html('');
+	$('#formulaInput').blur();
 }
 
 function loadSideBar(id){
 	var label = nodes[id].label;
 	var formula = nodes[id].formula;
-	$("#label").val(label);
-	$("#formulaInput").val(formula);
-	$("#inputsList").html("");
+	$('#label').val(label);
+	$('#formulaInput').val(formula);
+	$('#inputsList').html('');
 	for(let id of nodes[id].inputs){
 		let c = nodes[id].color;
 		let l = nodes[id].label;
-		$("#inputsList").append(
-			"<li onclick=\"addToFormula('"+l+"')\" class=\""+c+"\">"+l+"</li>"
+		$('#inputsList').append(
+			'<li onclick="addToFormula(\''+l+'\')" class="'+c+'">'+l+'</li>'
 		);
 	}
 }
 
 $(window).resize(function(){
 	for(let id of Object.keys(nodes)){
-		$("#"+id).css('top',nodes[id].top);
-		$("#"+id).css('left',nodes[id].left);
+		$('#'+id).css('top',nodes[id].top);
+		$('#'+id).css('left',nodes[id].left);
 		updateEdges(id);
 	}
 });
 
-$("#label").keydown(function(e){
+$('#label').keydown(function(e){
 	if(e.which === 13){
 		e.stopPropagation();
 		e.currentTarget.blur();
 	}
 });
-$("#label").keyup(function(e){
-	setLabel(selectedCell, $("#label").val());
+$('#label').keyup(function(e){
+	setLabel(selectedCell, $('#label').val());
 });
-$("#formulaInput").keydown(function(e){
+$('#formulaInput').keydown(function(e){
 	if(e.which === 13){
 		e.stopPropagation();
 		e.currentTarget.blur();
 	}
 });
-$("#formulaInput").keyup(function(e){
-	setFormula(selectedCell, $("#formulaInput").val());
+$('#formulaInput').keyup(function(e){
+	setFormula(selectedCell, $('#formulaInput').val());
 });
 
-$("#trash").droppable({
-	hoverClass: "not-transparent",
+$('#trash').droppable({
+	hoverClass: 'not-transparent',
     drop: function( event, ui ) {
     	destroyCell(ui.draggable.get(0).id);
     }
 });
-$(".wrapper").dblclick(function(e){
+$('.wrapper').dblclick(function(e){
 	createCell(e.pageX, e.pageY);
 });
-$(".wrapper").click(function(e){
+$('.wrapper').click(function(e){
 	//only unselect if they clicked blank area
-	if(e.originalEvent.target.className === "wrapper"){
+	if(e.originalEvent.target.className.indexOf('wrapper') > -1){
 		unselectCell();
 	}
 });
